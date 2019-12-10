d23cf9467ecf29e874a6fd9aabf08280f80c9824
AdrianaAriza <1296@holbertonschool.com>
2019-12-10 00:22:06 -0500

    [Fix]: lsits

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -21,6 +21,5 @@ size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
-listint_t **_realloc(listint_t **ptr, unsigned int old_size, unsigned int new_size);
 
 #endif /* LISTS_H */
