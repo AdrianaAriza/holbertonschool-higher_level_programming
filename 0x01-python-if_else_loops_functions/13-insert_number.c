8f925af4366eeba163ac4fefcef03843b9206994
Adriana Ariza <12986@holbertonschool.com>
2019-12-11 01:03:30 -0500

    [Fix]: If *head NULL

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -10,7 +10,7 @@ listint_t *insert_node(listint_t **head, int number)
 	listint_t *aux, *new;
 	unsigned int i = 0;
 
-	if (!head)
+	if (!head || !*head)
 		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (!new)
