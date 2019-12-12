26af3d87a73e97266a01110a82e6d41be28d484d
Adriana Ariza <12986@holbertonschool.com>
2019-12-12 16:27:00 -0500

    [Fix]: ooder

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     elif len(sys.argv) == 2:
         print(len(sys.argv) - 1, "argument:")
     else:
-        print("arguments:", len(sys.argv) - 1)
+        print(len(sys.argv) - 1, "arguments:")
     for i in range(1, len(sys.argv)):
         print("{}: {}".format(i, sys.argv[i]))
