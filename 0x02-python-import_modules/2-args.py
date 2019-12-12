d715ac4cb743a8d8ed228d7cab79c6f3faf0818a
Adriana Ariza <12986@holbertonschool.com>
2019-12-12 16:18:39 -0500

    [Fix]: :no space

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,4 +8,4 @@ if __name__ == "__main__":
     else:
         print("arguments:", len(sys.argv) - 1)
     for i in range(1, len(sys.argv)):
-        print(i, ": ", sys.argv[i])
+        print("{}: {}".format(i, sys.argv[i]))
