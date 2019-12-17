ba128e804f844a3dd4484785e0645866feec0ed9
Adriana Ariza <12986@holbertonschool.com>
2019-12-17 15:48:46 -0500

    [Fix]: sapces

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,7 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for k, j in enumerate(i):
-            if k < len(i):
+            if k != (len(i) - 1):
                 print("{:d} ".format(j), end="")
             else:
                 print("{:d}".format(j), end="")
