b5301da520b0517741020b17c686df512772473d
Adriana Ariza <1296@holbertonschool.com>
2020-01-08 15:31:44 -0500

    [Fix]: print any type of element

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             if c < x:
                 c += 1
-                print("{:d}".format(i), end='')
+                print(i, end='')
         except ValueError:
             print('')
             return c
