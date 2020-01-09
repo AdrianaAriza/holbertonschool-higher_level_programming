b0881379d60e165c2dd307296fbd695e020b4d2f
Adriana Ariza <1296@holbertonschool.com>
2020-01-08 21:38:27 -0500

    [Fix]: pep8

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,6 @@
 #!/usr/bin/pyhton3
-import sys
-
-
 def safe_print_integer_err(value):
+    import sys
     s = "Exception: Unknown format code 'd' for object of type 'str'"
     try:
         print("{:d}".format(value))
