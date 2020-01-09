6e3652a02a416d84cb10337ddd097e88db96aa5b
Adriana Ariza <1296@holbertonschool.com>
2020-01-08 22:26:34 -0500

    [Fix]: error syntax

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -4,6 +4,6 @@ def safe_function(fct, *args):
     try:
         r = fct(*args)
     except Exception as i:
-        sys.stderr.write("Exception:{}\n".format(i))
+        sys.stderr.write("Exception: {}\n".format(i))
         return None
     return r
