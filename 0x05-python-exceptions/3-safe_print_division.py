f84cbd6d728c2548d2a6080d155fc33bbcfe3090
Adriana Ariza <1296@holbertonschool.com>
2020-01-08 20:19:01 -0500

    [Fix]: exception

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     r = 0.0
+    f = 0
     try:
         r = a / b
     except ZeroDivisionError:
-        return None
+        f = 1
     finally:
-        print("Inside result: {:.1f}".format(r))
-    return r
+        if f is 0:
+            print("Inside result: {:.1f}".format(r))
+            return r
+        else:
+            print("Inside result: None")
+            return None
