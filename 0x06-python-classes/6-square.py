adb0d6a1f9c296ddf14037f86872ba9d4a2c96c2
Adriana Ariza <1296@holbertonschool.com>
2020-01-09 17:28:52 -0500

    [Fix]: pep8

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -28,15 +28,15 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
-        def area(self):
-            return self.__size*self.__size
+    def area(self):
+        return self.__size*self.__size
 
     def my_print(self):
         if self.__size == 0:
-            print ("")
+            print("")
         else:
             if self.position[1] > 0:
-                print ("" * self.__position[0])
+                print("" * self.__position[0])
             for i in range(self.__size):
-                print (' ' * self.__position[0], end='')
-                print ('#' * self.__size)
+                print(' ' * self.__position[0], end='')
+                print('#' * self.__size)
