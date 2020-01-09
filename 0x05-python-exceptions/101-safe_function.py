415b880304a062a55f5699892dcf87b6ac901a9f
Adriana Ariza <1296@holbertonschool.com>
2020-01-08 22:05:11 -0500

    [Fix]: Return

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,4 +5,4 @@ def safe_function(fct, *args):
         return fct(args[0], args[1])
     except Exception as i:
         sys.stderr.write("Exception:{}\n".format(i))
-        return None
+    return None
