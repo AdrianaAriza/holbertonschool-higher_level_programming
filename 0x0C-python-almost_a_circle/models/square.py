91bf905e959cb81bcd0da50664ebe7a0f64323da
Adriana Ariza <1296@holbertonschool.com>
2020-01-27 21:59:44 -0500

    [Fix]: Docuemntation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,30 +1,47 @@
 #!/usr/bin/python3
+"""
+square
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-
+    """
+    class
+    """
     def __init__(self, size, x=0, y=0, id=None):
         Rectangle.__init__(self, size, size, x, y, id)
         self.size = size
 
     @property
     def size(self):
+        """
+        size
+        """
         return self.width
 
     @size.setter
     def size(self, value):
+        """
+        size
+        """
         self.width = value
         self.height = value
 
     def __str__(self):
+        """
+        str
+        """
         s = "[Square] " + "(" + str(self.id) + ") " + str(self.x) + "/"
         s = s + str(self.y) + " - " + str(self.size)
         return s
 
     def update(self, *args, **kwargs):
+        """
+        update
+        """
         arg_l = ["id", "size", "x", "y"]
-        if args != None and len(args) != 0:
+        if args is not None and len(args) != 0:
             for i in range(len(args)):
                 setattr(self, arg_l[i], args[i])
         else:
@@ -32,6 +49,9 @@ class Square(Rectangle):
                 setattr(self, k, v)
 
     def to_dictionary(self):
+        """
+        to_dictionary
+        """
         l_att = ["id", "size", "x", "y"]
         l_val = [self.id, self.size, self.x, self.y]
         return dict(zip(l_att, l_val))
