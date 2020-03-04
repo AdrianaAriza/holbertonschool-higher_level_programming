87dca0ecb43bcf8dd2e38108aa9af31c1d3a8339
AdrianaAriza <1296@holbertonschool.com>
2020-03-04 00:20:43 -0500

    [Upload]: Task101

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,8 @@
+--
+--
+SELECT DISTINCT tv_shows.title
+FROM tv_shows
+LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name <> "Comedy" OR tv_genres.name IS NULL
+ORDER BY tv_shows.title;
