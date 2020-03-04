5987ca777eca3fc9a1dc50effe9b216894eabfd5
AdrianaAriza <1296@holbertonschool.com>
2020-03-04 00:28:38 -0500

    [FIx]: no the las man

--- 0x0E-SQL_more_queries/101-not_a_comedy.sql
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -4,5 +4,10 @@ SELECT DISTINCT tv_shows.title
 FROM tv_shows
 LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_genres.name <> "Comedy" OR tv_genres.name IS NULL
+WHERE tv_shows.title NOT IN
+(SELECT tv_shows.title
+FROM tv_genres
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_genres.name = 'Comedy')
 ORDER BY tv_shows.title;
