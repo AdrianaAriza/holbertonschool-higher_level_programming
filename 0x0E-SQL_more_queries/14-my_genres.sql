453f21835f173a3cd3b12fddb2d46d7929d9f910
AdrianaAriza <1296@holbertonschool.com>
2020-03-03 23:16:00 -0500

    [Upload]: Task14

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,7 @@
 --
 --
-SELECT tv_genres.name FROM tv_shows
+SELECT tv_genres.name
+FROM tv_shows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-FULL OUTER JOIN tv_genres ON tv_show_genres.show_id = tv_genres.id
-WHERE tv_shows.id = 8
-ORDER BY tv_genres.name;
+JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_shows.id = 8;
