5ef170ccd945520c144167060b5a6d691181bc41
AdrianaAriza <1296@holbertonschool.com>
2020-03-03 23:51:47 -0500

    [Fix]: title

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,5 +4,5 @@ SELECT tv_genres.name
 FROM tv_shows
 LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 LEFT OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-WHERE tv_shows.id = 8
+WHERE tv_shows.title = 'Dexter'
 ORDER BY tv_genres.name;
