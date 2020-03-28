75afa4c1d9bc16ffbf86254aa2b7f14a28091e52
AdrianaAriza <1296@holbertonschool.com>
2020-03-28 18:36:52 -0500

    [Fix]: case sensitive

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=usr, passwd=pwd, db=db_name)
     c = db.cursor()
     c.execute("""SELECT * FROM states
-            WHERE states.name LIKE 'N%'
+            WHERE states.name LIKE BINARY'N%'
             ORDER BY states.id ASC;""")
     R = c.fetchall()
     for i in R:
