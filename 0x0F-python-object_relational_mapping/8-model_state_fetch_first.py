e2defd26260d629ea83def431f93a0fd894c19d9
AdrianaAriza <1296@holbertonschool.com>
2020-03-29 13:08:44 -0500

    [Fix]: if none

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -13,5 +13,8 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     states = session.query(State).order_by(State.id).first()
-    print("{}: {}".format(states.id, states.name))
+    if states is None:
+        print()
+    else:
+        print("{}: {}".format(states.id, states.name))
     session.close()
