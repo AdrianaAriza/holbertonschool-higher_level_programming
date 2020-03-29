429e4f74679408f8882df6c827116636b8165184
AdrianaAriza <1296@holbertonschool.com>
2020-03-29 14:34:56 -0500

    [Upload]: Task12

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Start link class to table in database
+"""
+if __name__ == "__main__":
+    import sys
+    from sqlalchemy import create_engine
+    from model_state import Base, State
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
+                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    state = session.query(State).filter(State.id.like("2")).first()
+    state.name = "New Mexico"
+    session.commit()
