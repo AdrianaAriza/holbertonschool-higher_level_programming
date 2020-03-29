d73780d30b5ac67b4b6033b4925c47965a6d38b7
AdrianaAriza <1296@holbertonschool.com>
2020-03-28 19:18:41 -0500

    [Fix]: no print

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,7 +6,6 @@ if __name__ == "__main__":
     pwd = sys.argv[2]
     db_name = sys.argv[3]
     s_name = sys.argv[4]
-    print(type(s_name))
     db = MySQLdb.connect(user=usr, passwd=pwd, db=db_name)
     c = db.cursor()
     c.execute("""SELECT * FROM states
