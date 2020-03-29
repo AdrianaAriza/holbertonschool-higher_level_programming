b267eaba5ef833aee21f7c7649366e78e1c1846b
AdrianaAriza <1296@holbertonschool.com>
2020-03-28 19:17:47 -0500

    [Fix]: cas-sensitve

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -6,10 +6,11 @@ if __name__ == "__main__":
     pwd = sys.argv[2]
     db_name = sys.argv[3]
     s_name = sys.argv[4]
+    print(type(s_name))
     db = MySQLdb.connect(user=usr, passwd=pwd, db=db_name)
     c = db.cursor()
     c.execute("""SELECT * FROM states
-            LIKE BINARY name = "{:s}"
+            WHERE name LIKE BINARY "{:s}"
             ORDER BY states.id ASC;""".format(s_name))
     R = c.fetchall()
     for i in R:
