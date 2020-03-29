2116dc62676cce4ad1c30e6972d001b1fc57ad10
AdrianaAriza <1296@holbertonschool.com>
2020-03-28 23:20:32 -0500

    [Fix]: many

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 """
 """
-from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
 
@@ -11,6 +10,6 @@ Base = declarative_base()
 class State(Base):
     """
     """
-    __tableame__ = states
-    id = Column(Integer, primary_key=True, autoincrement=Ture, nullable=False)
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
