f164a33018f0359e979453af395304872d500e0c
AdrianaAriza <1296@holbertonschool.com>
2020-04-09 20:13:51 -0500

    [Fix]: space

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# accepted methos
-curl -sI -X OPTIONS "$1" | grep Allow: | awk '{print $2 $3 $4}'
+#!/usr/bin/bash
+#
+curl -sI -X OPTIONS "$1" | awk '/Allow:/ {print $2 " " $3 " " $4}'
