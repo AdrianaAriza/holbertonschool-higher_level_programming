7e610c6e58a47c66be1b660eb9cd51e39468068f
AdrianaAriza <1296@holbertonschool.com>
2020-04-15 17:26:22 -0500

    [Upload]: Task2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    import sys
+    import urllib.parse
+    import urllib.request
+
+    url = sys.argv[1]
+    values = {'email': sys.argv[2]}
+    data = urllib.parse.urlencode(values)
+    data = data.encode('ascii')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))
