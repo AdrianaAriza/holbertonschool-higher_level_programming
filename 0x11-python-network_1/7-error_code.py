df1fbf27b849691ab8467f35d107d13ddd78d282
AdrianaAriza <1296@holbertonschool.com>
2020-04-15 18:14:51 -0500

    [Upload]: Task7

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    r = requests.post(sys.argv[1])
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
+    else:
+        print(r.text)
+
