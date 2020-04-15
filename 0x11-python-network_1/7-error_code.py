29feddf2b34522bd3e3f19a93e338b9c35775f53
AdrianaAriza <1296@holbertonschool.com>
2020-04-15 18:34:40 -0500

    [Fix]: get/post

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    r = requests.post(sys.argv[1])
+    r = requests.get(sys.argv[1])
     if r.status_code >= 400:
         print("Error code: {}".format(r.status_code))
     else:
