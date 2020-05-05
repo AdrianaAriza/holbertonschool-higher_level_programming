0fc40e8e51e0008e091db61bcb78a8c71a4e91a6
AdrianaAriza <1296@holbertonschool.com>
2020-05-05 14:53:05 -0500

    [Fix]: Style

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,8 +3,7 @@ const arr = 'C is fun';
 let i = 0;
 if (isNaN(parseInt(process.argv[2]))) {
   console.log('Missing number of occurrences');
-}
-else {
+} else {
   for (i; i < parseInt(process.argv[2]); i++) {
     console.log(arr);
   }
