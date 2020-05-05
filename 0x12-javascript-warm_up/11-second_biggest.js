44bdaad3b6d3ea20c1a26c8df25295c53f5673d8
AdrianaAriza <1296@holbertonschool.com>
2020-05-05 17:45:36 -0500

    [Upload]: Task11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const arr = [];
+const len = process.argv.length;
+let i = 0;
+if (len === 2 || len === 3) {
+  console.log(0);
+} else {
+  for (i = 2; i < len; i++) {
+    arr.push(process.argv[i]);
+  }
+  arr.sort().reverse();
+  i = 0;
+  while (arr[i] === arr[i + 1]) {
+    i++;
+  }
+  console.log(arr[i + 1]);
+}
