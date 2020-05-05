8d8318080c2998ee8d868da1ff78f5b2bac8fc07
AdrianaAriza <1296@holbertonschool.com>
2020-05-05 17:14:26 -0500

    [Fix]: fucntion

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 let f = 1;
 let n = parseInt(process.argv[2]);
+function fact (n, f) {
+  return f * n;
+}
 while (n > 0) {
-  f = f * n;
+  f = fact(n, f);
   n--;
 }
 console.log(f);
