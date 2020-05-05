1876d3e85be4a07ef82625e0687c9e8545b51c08
AdrianaAriza <1296@holbertonschool.com>
2020-05-05 13:11:12 -0500

    [Upload]: Task3

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 if (process.argv.length == 2) {
-    console.log('No argument')
+    console.log('No argument');
 }
 if (process.argv.length == 3) {
-    console.log('Argument found')
+    console.log('Argument found');
 }
 if (process.argv.length > 3) {
   console.log('Arguments found');
