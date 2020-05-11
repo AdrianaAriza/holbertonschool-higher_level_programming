c5e24a22ec8c1cae36741b035ca5ad27214000b3
AdrianaAriza <1296@holbertonschool.com>
2020-05-11 18:49:52 -0500

    [Fix]:

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,11 +2,6 @@
 class Rectangle {
   constructor (w, h) {
     [this.width, this.height] = [w, h];
-    Rectangle.print = function () {
-      for (x = 0; x < h; x++){
-        console.log('X'* w);
-      }
-    }
   }
 }
 module.exports = Rectangle;
