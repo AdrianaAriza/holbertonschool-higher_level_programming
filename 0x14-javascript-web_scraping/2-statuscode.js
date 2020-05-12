fc1f7685ad0edb8b478bae0a937bf94a3d7d6512
AdrianaAriza <1296@holbertonschool.com>
2020-05-12 17:00:28 -0500

    [Fix]: handle error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(process.argv[2], function (res) {
+request(process.argv[2], function (err, res) {
+  if (err) {
+    console.log('code: ' + err.statusCode);
+    return;
+  }
   console.log('code: ' + res.statusCode);
 });
