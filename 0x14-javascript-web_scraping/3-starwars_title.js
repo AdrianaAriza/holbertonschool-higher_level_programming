3ccd016479a714d47b3f9b251eee8667cffa2edc
AdrianaAriza <1296@holbertonschool.com>
2020-05-12 17:59:36 -0500

    [Upgrade]: json

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,8 +4,8 @@ const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, function (err, res, body) {
   if (res) {
-    const title = body.split(':')[1].split(',')[0].split('"')[1];
-    console.log(title);
+    const title = JSON.parse(body);
+    console.log(title.title);
     return;
   }
   console.log(err);
