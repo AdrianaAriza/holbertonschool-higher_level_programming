fccb92de19b749df71c2d6b6548b5bf2dea6f4de
AdrianaAriza <1296@holbertonschool.com>
2020-05-12 21:15:15 -0500

    [Fix]: argv

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/films/';
+const url = process.argv[2];
 let cont = 0;
 
 request(url, function (err, res, body) {
-  if (err === null) {
+  if (res) {
     const title = JSON.parse(body);
     const list = title.results.map(ele => ele.characters);
     list.forEach(e => e.includes('https://swapi-api.hbtn.io/api/people/18/') ||
