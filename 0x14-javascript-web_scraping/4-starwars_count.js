9cbd9ed7d70a2e7e54bb8c4181ca5a12da1832e3
AdrianaAriza <1296@holbertonschool.com>
2020-05-12 20:54:23 -0500

    [Fix]: hande error

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,9 +3,13 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/';
 let cont = 0;
 
-request(url, function (_err, _res, body) {
-  const title = JSON.parse(body);
-  const list = title.results.map(ele => ele.characters);
-  list.forEach(e => e.includes('https://swapi-api.hbtn.io/api/people/18/') ? cont++ : '');
-  console.log(cont);
+request(url, function (err, res, body) {
+  if (res){
+    const title = JSON.parse(body);
+    const list = title.results.map(ele => ele.characters);
+    list.forEach(e => e.includes('https://swapi-api.hbtn.io/api/people/18/') ? cont++ : '');
+    console.log(cont);
+    return;
+  }
+  console.log(err);
 });
