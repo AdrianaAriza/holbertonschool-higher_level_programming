4fe0afbcfea645ae13cc4942efce35c9d5ad7196
AdrianaAriza <1296@holbertonschool.com>
2020-05-12 21:40:12 -0500

    [Fix]: all url

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,13 +2,13 @@
 const request = require('request');
 const url = process.argv[2];
 let cont = 0;
+var x = /18/;
 
 request(url, function (err, res, body) {
   if (res) {
     const title = JSON.parse(body);
     const list = title.results.map(ele => ele.characters);
-    list.forEach(e => e.includes('https://swapi-api.hbtn.io/api/people/18/') ||
-    e.includes('http://swapi.co/api/people/18/') ? cont++ : '');
+    list.forEach(e => x.test(e) ? cont++ : '');
     console.log(cont);
     return;
   }
