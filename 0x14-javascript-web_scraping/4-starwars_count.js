f5f77edb49d11634db624866931d152e25c11dbc
AdrianaAriza <1296@holbertonschool.com>
2020-05-12 20:19:20 -0500

    [Upload]: Task4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 const request = require('request');
-const url = 'https://swapi-api.hbtn.io/api/people/18';
+const url = 'https://swapi-api.hbtn.io/api/films/';
+let cont = 0;
 
 request(url, function (_err, _res, body) {
   const title = JSON.parse(body);
-  console.log(title.films.length);
+  const list = title.results.map(ele => ele.characters);
+  list.forEach(e => e.includes('https://swapi-api.hbtn.io/api/people/18/') ? cont++ : '');
+  console.log(cont);
 });
